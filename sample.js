ea06226c69215e2c6cac1c4f0b717d2dea495847
tweeeety <tweeeety.0719@gmail.com>
2014-08-13 19:56:29 +0900

    rawgit
    rawgit

--- sample.js
+++ sample.js
@@ -1,3 +1,12 @@
+// ノード追加
+var elm = document.createElement('div');
+for(var i=0; i<5; i++){
+  var e = elm.cloneNode();
+  e.textContent = 'サンプルノード' + i;
+  document.body.appendChild(e);
+}
+
+// console
 for(var i=0; i<10; i++){
   console.log("サンプルだよ : " + i);
 }
